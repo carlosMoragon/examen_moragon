6ade17556382f38271d6555f35e4c909bf2221dc
Carlos Moragon <cmoragoncorella@gmail.com>
2022-05-06 08:59:33 +0200

    terminado ejercicio 1

---
+++ ejercicio1/src/main/java/com/moragon/ejercicio1/Ejercicio1Application.java
@@ -0,0 +1,38 @@
+package com.moragon.ejercicio1;
+
+
+import org.jetbrains.annotations.Contract;
+import org.jetbrains.annotations.NotNull;
+
+public class Ejercicio1Application {
+
+    public static void main(String[] args) {
+        /*Una tabla y funciones
+        Escriba una función que cree una tabla que tenga una dimensión de diez números enteros e inicialice aleatoriamente los valores de esta tabla, con valores entre un límite mínimo y un límite máximo, que se pasan como argumentos.
+        Cree una función que devuelva el mayor valor de una tabla (esta tabla no tiene necesariamente diez celdas, etc.).
+                Escriba un algoritmo que llame a estas dos funciones.*/
+        System.out.println(mayorNumero(devolverTabla()));
+
+    }
+
+    private static int @NotNull [] devolverTabla(){
+        int[] tabla = new int[10];
+        for (int i = 0; i < tabla.length; i++) {
+            tabla[i] = (int) (Math.random() * 10);
+            //System.out.println(tabla[i]);
+        }
+        return tabla;
+    }
+    @Contract(pure = true)
+    private static int mayorNumero(int @NotNull [] tabla){
+        int mayor = 0;
+        for (int i = 0; i < tabla.length; i++) {
+            if(tabla[i]> mayor){
+                mayor = tabla[i];
+            }
+        }
+        return mayor;
+    }
+}
+
+
